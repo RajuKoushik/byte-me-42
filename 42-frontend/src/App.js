6bb117c74f57dce89957c03922301738c210ded7
AkashKadam75 <kadamakash75@gmail.com>
2018-10-01 15:04:29 -0700

    singup page component added

--- 42-frontend/src/App.js
+++ 42-frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import LoginLayout from './components/login_layout';
+import SignUp from './components/signUp';
 import './App.css';
-import './components/login_layout.css';
+import './components/signUp.css';
 import HomepageLayout from './components/homepage_layout';
 import SearchBar from './components/homepage_search';
 import Categories from './components/homepage_categories';
@@ -11,11 +11,7 @@ class App extends Component {
     render(){
         return(
             <div>
-                <LoginLayout />
-                <HomepageLayout />
-                <SearchBar />
-                <Categories />
-                <Companies />
+                <SignUp />
             </div>
         );
     }
