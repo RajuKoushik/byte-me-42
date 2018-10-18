80995f5b2e0d5706dde03bed129f1f9a915a5e69
Trupti Khatavkar <khatavkar.trupti@gmail.com>
2018-10-18 09:03:38 -0700

    Sprint 2 User story 8 task 30

--- 42-frontend/src/App.js
+++ 42-frontend/src/App.js
@@ -1,23 +1,15 @@
 import React, { Component } from 'react';
-import LoginLayout from './components/login_layout';
 import './App.css';
-import './components/login_layout.css';
-import HomepageLayout from './components/homepage_layout';
-import SearchBar from './components/homepage_search';
-import Categories from './components/homepage_categories';
-import Companies from './components/homepage_companies';
-import Header from './components/homepage_header';
+import User_profile from './components/user_profile';
+import './components/user_profile.css';
+
 
 class App extends Component {
     render(){
         return(
             <div>
-                <LoginLayout />
-                <HomepageLayout />
-                <SearchBar />
-                <Categories />
-                <Companies />
-                <Header />
+
+                <User_profile/>
             </div>
         );
     }
---
+++ 42-frontend/src/components/user_profile.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './user_profile.css';
+
+class User_profile extends Component{
+    render(){
+        return(
+            <body>
+            <h1>Sample 42</h1>
+            <label id="likes">Likes</label>
+            </body>
+        );
+    }
+}
+
+
+export default User_profile;
