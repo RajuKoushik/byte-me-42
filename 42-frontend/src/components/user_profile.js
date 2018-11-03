05d615425944d80b329967ee99f796f886979de2
Trupti Khatavkar <khatavkar.trupti@gmail.com>
2018-11-03 15:11:56 -0700

    Sprint 3 User story #9 task #47

--- 42-frontend/src/components/user_profile.js
+++ 42-frontend/src/components/user_profile.js
@@ -2,18 +2,17 @@ import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './user_profile.css';
 import Posts from "./posts";
-
+import Followers from "./followers";
+import Followings from "./following";
 
 class User_profile extends Component{
     render(){
         return(
             <body>
             <form>
-            <label id="name">First Last</label>
-            <label id="followers">Followers</label>
-                <label id="followers_no">0</label>
-            <label id="following">Following</label>
-                <label id="following_no">0</label>
+            <label id="name">Sample Name</label>
+            <Followers/>
+            <Followings/>
             <label id="posts">Posts</label>
                 <label id="posts_no">0</label>
             </form>
