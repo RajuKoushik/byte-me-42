69bc8368bd400037451dab9ad434215e5a27605b
Manikanta Chintakunta <manikanta1596@gmail.com>
2018-11-04 17:41:57 -0700

    Additional updates with backend42/urls

--- backend42/backend42/urls.py
+++ backend42/backend42/urls.py
@@ -13,8 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
+
+from blog.views import home, user_profile, follow
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
