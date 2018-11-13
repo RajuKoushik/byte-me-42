c19dd2da497e7c854e8ee31e2dc52f2b520d00db
Kjha7 <43060657+Kjha7@users.noreply.github.com>
2018-11-13 05:01:49 -0700

    Update urls.py made systematic for frront end integration

--- backend42/blog/urls.py
+++ backend42/blog/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 from django.views.generic.base import RedirectView
 from blog import views
-from django.views.decorators.csrf import csrf_exempt
 
 app_name = 'blog'
 
@@ -9,22 +8,10 @@ favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     url(r'^favicon\.ico$', favicon_view),
-    url(r'^test/$', views.homeRest, name='rest'),
-    url(r'^testSerializer/$', views.homeSerializer.as_view()),
-
-    url(r'^testSerializer/$', views.homeSerializer.as_view()),
-
     url(r'^(?P<post_id>.+)/like/(?P<username>.+)/$', views.like, name='like'),
-    url(r'^post/(?P<post_id>.+)/$', views.Post_view.as_view(),
-        name='post_view'),
+    url(r'^post/(?P<post_id>.+)/$', views.post_view, name='post_view'),
     url(r'^create/post/$', views.create_post, name='create_post'),
-    url(r'^create/newpost/$', csrf_exempt(views.new_post_create), name='create_post'),
     url(r'^edit/post/(?P<post_id>.+)/$', views.post_edit, name='post_edit'),
     url(r'^fork/post/(?P<post_id>.+)/$', views.fork, name='fork'),
-    url(r'^delete/post/(?P<post_id>.+)/$', views.post_delete,
-        name='post_delete'),
-    url(r'^all_post/$', views.AllPost.as_view()),
-    url(r'^user/(?P<username>.+)', views.ProfileREST.as_view(),
-        name='user_profile'),
-
+    url(r'^delete/post/(?P<post_id>.+)/$', views.post_delete, name='post_delete')
 ]
