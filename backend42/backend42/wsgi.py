cdb4f38189124a75197b3a6f18c653d2c2aafbb7
Manikanta Chintakunta <manikanta1596@gmail.com>
2018-11-17 00:48:52 -0700

    Deployment on new branch

--- backend42/backend42/wsgi.py
+++ backend42/backend42/wsgi.py
@@ -6,11 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
-
 import os
 
+from django.core.wsgi import get_wsgi_application	
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend42.settings")
-
-application = get_wsgi_application()
+from whitenoise.django import DjangoWhiteNoise
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend42.settings")	os.environ.setdefault(“DJANGO_SETTINGS_MODULE”, “the-42-django-react.settings”)
+application = get_wsgi_application()	application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
