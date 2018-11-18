29b0e5901e3fe08f21052fdb05c9c44e9ea62e8b
Carnic <ca108@snu.edu.in>
2018-11-17 23:47:57 -0700

    sprint 4 User story13 task 72, added forking from branch

--- 42-frontend/src/components/Branch.js
+++ 42-frontend/src/components/Branch.js
@@ -1,48 +1,174 @@
-import React from 'react';
-import { List, Card, Icon,Avatar,Button } from 'antd';
+import React, {Component} from 'react';
+import { List, Avatar, Icon } from 'antd';
+import { Popover, Button } from 'antd';
+import { Drawer, Form, Col, Row, Input, Select, DatePicker } from 'antd';
+import * as props from "antd";
+import * as ReactDOM from "antd";
+
+
+const { Option } = Select;
 
 const IconText = ({ type, text }) => (
-  <span>
+    <span>
     <Icon type={type} style={{ marginRight: 8 }} />
-    {text}
+        {text}
   </span>
 );
 
-const Branch = (props) => {
-	console.log("props data"+props.branchnumber);
-    return (
-    <div>
-    <List
-        itemLayout="vertical"
-        size="medium"
-        grid={{ gutter: 16, column: 1 }}
-        dataSource={props.data}
-        renderItem={item => (
-        <List.Item
-            key={item.title}
-            actions={[<IconText type="star-o" text="156" />, <IconText type="like-o" text="156" />, <IconText type="message" text="2" />]}
-            extra={<img width={272} alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />}
-        >
-            <List.Item.Meta
-            avatar={<Avatar src={item.avatar} />}
-            title={<a href={`/branch/${item.id}`}>{item.title}</a>}
-            description={item.author}
-            />
-            {item.content}
-        </List.Item>
-        )}
-    />
-    <List
-	    grid={{ gutter: 16, xs: 1, sm: 1, md: 1, lg: 1, xl: 20, xxl: 3 }}
-	    dataSource={props.branchnumber}
-	    renderItem={item => (
-	      <List.Item>
-	        <Button href={`/branch/${item.firstPostId}/${item.branchIndex}`} type="primary">{item.branchIndex}</Button>
-	      </List.Item>
-	    )}
-	 />
-    </div>
-    )
+const buttonStyle = {
+    opacity : '0.4',
+    position : 'absolute',
+    marginTop : '5%',
+    backgroundColor : '#581845'
+
 }
 
-export default Branch;
+const postStyle = {
+    width : '80%'
+}
+
+
+class Branch extends React.Component {
+    state = { visible: false };
+
+    constructor (props){
+        super(props)
+    }
+
+    state = {
+        visible: false,
+    }
+
+    hide = () => {
+        this.setState({
+            visible: false,
+        });
+    }
+
+    handleVisibleChange = (visible) => {
+        this.setState({ visible });
+    }
+
+    showDrawer = () => {
+        this.setState({
+            visible: true,
+        });
+    };
+
+    onClose = () => {
+        this.setState({
+            visible: false,
+        });
+    };
+
+    handleFormSubmit = (event, origin_id) => {
+        //const title = event.target.elements.title.value;
+        const content = event.target.elements.content.value;
+        //const user_id = localStorage.getItem('token');
+        console.log(content);
+    }
+
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        return (
+            <div>
+                <List
+
+                    itemLayout="vertical"
+                    size="large"
+                    dataSource={this.props.data}
+                    renderItem={item => (
+                        <List.Item
+                            key={item.title}
+                            actions={[<IconText type="star-o" text="156"/>]}
+
+                        >
+                            <List.Item.Meta
+                                avatar={<Avatar src={item.avatar}/>}
+                                //title={<a href={`/branch/${item.id}`}>{item.title}</a>}
+                                description={item.author}
+                            />
+
+                            <div>
+                                <Button type="primary" style = {buttonStyle} onClick={this.showDrawer}>
+                                    Fork
+                                </Button>
+                                <Drawer
+                                    title="Fork"
+                                    width={420}
+                                    placement="right"
+                                    onClose={this.onClose}
+                                    maskClosable={false}
+                                    visible={this.state.visible}
+                                    style={{
+                                        height: 'calc(100% - 55px)',
+                                        overflow: 'auto',
+                                        paddingBottom: 53,
+                                    }}
+                                >
+                                    <Form layout="vertical" hideRequiredMark onSubmit={(event) => this.handleFormSubmit(
+                                        event,
+                                        item.id)}>
+                                        <Row gutter={16} style={postStyle}>
+                                            <Col span={24}>
+                                                <Form.Item label="content" >
+                                                    {getFieldDecorator('content',
+                                                        {
+                                                            rules: [
+                                                                {
+                                                                    required: true,
+                                                                    message: 'please enter your content',
+                                                                },
+                                                            ],
+                                                        })(<Input.TextArea name="content" rows={4} placeholder="please enter your content" />)}
+                                                </Form.Item>
+                                            </Col>
+                                        </Row>
+                                    </Form>
+                                    <div
+                                        style={{
+                                            position: 'absolute',
+                                            bottom: 0,
+                                            width: '100%',
+                                            borderTop: '1px solid #e8e8e8',
+                                            padding: '10px 16px',
+                                            textAlign: 'right',
+                                            left: 0,
+                                            background: '#fff',
+                                            borderRadius: '0 0 4px 4px',
+                                        }}
+                                    >
+                                        <Button
+                                            style={{
+                                                marginRight: 8,
+                                            }}
+                                            onClick={this.onClose}
+                                        >
+                                            Cancel
+                                        </Button>
+                                        <Button onClick={this.onClose} type="primary">Submit</Button>
+                                    </div>
+                                </Drawer>
+                            </div>
+                            {item.content}
+                        </List.Item>
+
+                    )}
+
+                />
+
+
+            </div>
+
+
+
+
+        );
+    }
+}
+
+const App = Form.create()(Branch);
+
+//ReactDOM.render(<App />, mountNode);
+
+export default App;
